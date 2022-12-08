d31c1d1437c99ab0cc56ddbc86ec1610105d1c16
fdz21 <francodzalazar@gmail.com>
2022-12-08 20:47:23 -0300

    detalles items

--- my-app/src/components/Item/Item.jsx
+++ my-app/src/components/Item/Item.jsx
@@ -5,8 +5,8 @@ const Item = ({prod}) => {
         <div className='card mb-3 cardProducto border-light'>
                         <img src={`../img/${prod.img}`}className="card-img-top" alt="..." />
                         <div className='card-body cardBody'>
-                            <h5 className="card-title">{prod.nombre} {prod.modelo}</h5>
-                            <p className="card-text">*{prod.marca}*</p>
+                            <h5 className="card-title">{prod.nombre}</h5>
+                            <p className="card-text">{prod.plataforma}</p>
                             <p className="card-text">$ {new Intl.NumberFormat('de-DE').format(prod.precio)}</p>
                             <button className='btn btn-primary'><Link className="nav-link" to={`/product/${prod.id}`}>Ver Producto</Link></button>
                         </div>
